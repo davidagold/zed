70c33b2e859296584df3a8ea9c3eaf423614a577
David Gold <david.gold.a@gmail.com>
2024-05-04 04:53:45 -0700

    Start language server and focus handle/project item fussing

--- crates/notebook/src/common.rs
+++ crates/notebook/src/common.rs
@@ -1,7 +1,23 @@
-use serde::de::{self, DeserializeOwned, DeserializeSeed, Error, Visitor};
+use language::{Language, LanguageConfig, LanguageMatcher};
+use serde::de::DeserializeOwned;
+use tree_sitter_python;
 
 pub(crate) fn parse_value<T: DeserializeOwned, E: serde::de::Error>(
     val: serde_json::Value,
 ) -> Result<T, E> {
     serde_json::from_value::<T>(val).map_err(|err| E::custom(err.to_string()))
+}
+
+pub(crate) fn python_lang() -> Language {
+    Language::new(
+        LanguageConfig {
+            name: "Python".into(),
+            matcher: LanguageMatcher {
+                path_suffixes: vec!["rs".to_string()],
+                ..Default::default()
+            },
+            ..Default::default()
+        },
+        Some(tree_sitter_python::language()),
+    )
 }
