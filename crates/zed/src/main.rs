c4dbf81a32834f51c51d54a705af70bdb44c6bfe
David Gold <david.gold.a@gmail.com>
2024-05-13 06:49:32 -0700

    Run current notebook cell (#4)
    * Superfluous change that was nonetheless bugging me
    
    * Fill out, consolidate Cells initialization and SumTree dimensions
    
    * Commit work while I try to undo accidental find+replace-all
    
    * Work on updating outputs
    
    * Include Python modules
    
    * Small post-rebase fixes
    
    * Progress on adding new cell
    
    * Checkpoint
    
    * We can insert cells
    
    * We can insert cells
    
    * Wow that took much longer than expected
    
    * Get current cell ID from buffer
    
    * Include cell ID in KernelMessage event
    
    * Remove stale output of current cell when run
    
    * IT WORKS
    
    * Clean up a little bit

--- crates/zed/src/main.rs
+++ crates/zed/src/main.rs
@@ -134,6 +134,7 @@ fn init_ui(app_state: Arc<AppState>, cx: &mut AppContext) -> Result<()> {
     command_palette::init(cx);
     editor::init(cx);
     image_viewer::init(cx);
+    notebook::editor::init(cx);
     diagnostics::init(cx);
 
     audio::init(Assets, cx);
@@ -360,7 +361,6 @@ fn main() {
         project::Project::init(&client, cx);
         client::init(&client, cx);
         language::init(cx);
-        editor::init(cx);
 
         let telemetry = client.telemetry();
         telemetry.start(installation_id.clone(), session_id, cx);
